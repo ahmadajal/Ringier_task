3752b01576026fb1e3ebd2f72eb1eafaced46474
ahmadajal <ajal.ahmad@gmail.com>
2024-03-07 09:58:45 +0000

    add docstrings

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from typing import List
 
 import numpy as np
 import pandas as pd
@@ -16,7 +18,7 @@ from transformers import (
 
 from preprocess import preprocess_data
 
-logging.basicConfig(filename="train_log.log", encoding="utf-8", level=logging.DEBUG)
+logging.basicConfig(filename="train_log.log", encoding="utf-8")
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 TAXONOMY_PATH = "data/taxonomy_mappings.json"
 
@@ -25,7 +27,7 @@ class Training:
     def __init__(
         self, path_to_train: str, lr: float = 5e-5, batch_size: int = 16, epochs: int = 3
     ) -> None:
-        """_summary_
+        """The main training class.
 
         Args:
             path_to_train (str): _description_
@@ -38,7 +40,16 @@ class Training:
         self.batch_size = batch_size
         self.epochs = epochs
 
-    def load_datasets(self):
+    def load_datasets(self) -> List[Dataset]:
+        """
+        This method preprocess the raw data and convert it to Huggingface dataset. Then
+        it tokenize the dataset with the proper tokenizer and converts it to torch format.
+        Finally, it splits the data to training and test, maintining the balance of the
+        class labels in both datasets.
+
+        Returns:
+            List: train and test datasets.
+        """
         data = preprocess_data(
             path_to_data=self.path, test=False, path_to_taxonomy_mappings=TAXONOMY_PATH
         )
@@ -68,16 +79,17 @@ class Training:
         return train_dataset, test_dataset
 
     def train(self):
+        """This method loads the train and test datasets and trains the model."""
         X_train, X_test = self.load_datasets()
         logging.info("Training and Test data loaded.")
         num_labels = len(X_train[0]["labels"])
         train_dataloader = DataLoader(X_train, shuffle=True, batch_size=self.batch_size)
         test_dataloader = DataLoader(X_test, batch_size=self.batch_size)
-        model = AutoModelForSequenceClassification.from_pretrained(
+        self.model = AutoModelForSequenceClassification.from_pretrained(
             "google-bert/bert-base-cased", num_labels=num_labels
         )
         # Model optimizer and learning rate scheduler.
-        optimizer = AdamW(model.parameters(), lr=self.lr)
+        optimizer = AdamW(self.model.parameters(), lr=self.lr)
         lr_scheduler = get_scheduler(
             name="linear",
             optimizer=optimizer,
@@ -85,15 +97,15 @@ class Training:
             num_training_steps=self.epochs * len(train_dataloader),
         )
         # Put the model in the appropriate device.
-        model.to(device)
+        self.model.to(device)
         # Start the training loop.
         logging.info("Training starts ...")
         progress_bar = tqdm(range(self.epochs * len(train_dataloader)))
         for epoch in range(self.epochs):
-            model.train()
+            self.model.train()
             for batch in train_dataloader:
                 batch = {k: v.to(device) for k, v in batch.items()}
-                outputs = model(**batch)
+                outputs = self.model(**batch)
                 loss = outputs.loss
                 loss.backward()
 
@@ -101,20 +113,20 @@ class Training:
                 lr_scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
-            acc_epoch = self.evaluate(model, test_dataloader)
+            acc_epoch = self.evaluate(self.model, test_dataloader)
             logging.info(f"Validation accuracy after epoch {epoch}: {acc_epoch}")
         logging.info("Training finished!")
-        acc = self.evaluate(model, test_dataloader)
+        acc = self.evaluate(test_dataloader)
         logging.info(f"Test accuracy: {acc}")
         print(f"Test accuracy: {acc}")
 
-    def evaluate(self, model, test_dataloader):
+    def evaluate(self, test_dataloader):
         correct_preds = np.array([])
-        model.eval()
+        self.model.eval()
         for batch in test_dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             with torch.no_grad():
-                outputs = model(**batch)
+                outputs = self.model(**batch)
 
             logits = outputs.logits
             preds = torch.argmax(logits, dim=-1)
@@ -128,3 +140,5 @@ class Training:
 if __name__ == "__main__":
     training = Training(path_to_train="data/train_data.json")
     training.train()
+    os.makedirs("models/", exist_ok=True)
+    training.model.save_pretrained("models/bert-based-trained")
