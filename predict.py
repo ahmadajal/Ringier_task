c58b1c815a001fb5953b08da4769ba1fb0c79d24
ahmadajal <ajal.ahmad@gmail.com>
2024-03-09 12:34:30 +0000

    adapt predict.py to Trainer.

--- predict.py
+++ predict.py
@@ -1,17 +1,13 @@
 import argparse
 import json
-import logging
 import os
 from typing import List
 
-import torch
-from torch.utils.data import DataLoader
-from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
+from transformers import AutoModelForSequenceClassification, Trainer
 
-from train import Training
+from train import load_datasets
 
-logging.basicConfig(filename="test_log.log", encoding="utf-8")
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--path_to_payload", type=str, help="path to predict_payload.json")
 args = argparser.parse_args()
@@ -27,23 +23,19 @@ def predict(predict_payload: str) -> List:
         A list of lists of class probabilities.
     """
     # Load the tokenized test data.
-    training = Training(path_to_train=predict_payload)
-    pred_dataset = training.load_datasets(test=True)
-    logging.info("Prediction data loaded.")
-    # We need to load the data in batches, in case the prediction dataset is large!
-    pred_dataloader = DataLoader(pred_dataset, batch_size=training.batch_size)
+    pred_dataset = load_datasets(path=predict_payload, test=True)
     # Load the trained model
-    model = AutoModelForSequenceClassification.from_pretrained("models/bert-based-trained/")
-    model.to(device)
-    model.eval()
-    pred_probs = []
-    for batch in pred_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            output = model(**batch)
-        probabilities = torch.nn.Softmax(dim=1)(output.logits)
-        pred_probs.extend(probabilities.cpu().numpy().tolist())
-    return pred_probs
+    ckpt_dirs = os.listdir("models/bert-cased-trainer")
+    # As we loaded the best model in the end of training, the last checkpoint
+    # is the best one.
+    last_ckpt = sorted(ckpt_dirs, key=lambda x: int(x.split("-")[1]))[-1]
+    model = AutoModelForSequenceClassification.from_pretrained(
+        f"models/bert-cased-trainer/{last_ckpt}/"
+    )
+    trainer = Trainer(model)
+    output = trainer.predict(pred_dataset)
+    pred_probs = softmax(output.predictions, axis=1)
+    return pred_probs.tolist()
 
 
 if __name__ == "__main__":
@@ -52,3 +44,4 @@ if __name__ == "__main__":
     os.makedirs("predictions/", exist_ok=True)
     with open("predictions/probas.json", "w") as f:
         json.dump(pred_probs, f)
+    print("predictions are save to predictions/probas.json")
